2cb6995b5fa2b426d0f51985acdcae697aca9b2d
StefanOliveira <carlos.stefan86@yahoo.com.br>
2021-08-20 15:01:39 -0300

    alteração-variavel de endereço

--- PortScanner.py
+++ PortScanner.py
@@ -3,10 +3,10 @@ import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.settimeout(0.05)
 
-ip = input("Digite o endereço do Host: ")
+ipaddr = input("Digite o endereço do Host: ")
 port = int(input("Digite a porta a ser verificada: "))
 
-code = client.connect_ex((ip, port))
+code = client.connect_ex((ipaddr, port))
 
 if code == 0:
     print("Porta Aberta!")
